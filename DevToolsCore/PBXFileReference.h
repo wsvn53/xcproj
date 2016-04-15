055f8c1133d8d752fbfe077ca1b16a4a899ae4bc
Ethan <wsvn53@gmail.com>
2016-04-15 21:52:15 +0800

    fix initWithName.. return value

--- DevToolsCore/PBXFileReference.h
+++ DevToolsCore/PBXFileReference.h
@@ -2,6 +2,6 @@
 
 @protocol PBXFileReference <PBXReference, NSObject>
 
-- (id)initWithName:(id)arg1 path:(id)arg2 sourceTree:(id)arg3;
+- (id<PBXFileReference>)initWithName:(id)arg1 path:(id)arg2 sourceTree:(id)arg3;
 
 @end
